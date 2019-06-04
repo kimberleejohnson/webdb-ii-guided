a7321145a98ab4e78e80e9c8ba80a8b4d093905c
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-06-04 09:35:19 -0700

    Installed knex and sqlite, configured, ran first endpoint

--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,8 +1,28 @@
+const knex = require('knex');
+
 const router = require('express').Router();
 
+// 1. npm install knex and driver 
+// 2. configure knex, after importing up above, and get a connection to database
+// 3. Configure knexConfig object
+
+const knexConfig = {
+  client: 'sqlite3', 
+  connection: {
+    filename: './data/Rolex.db3'
+  },
+  useNullAsDefault: true, // required only for sqlite3 
+}
+
+const db = knex(knexConfig); 
+
 router.get('/', (req, res) => {
-  // get the roles from the database
-  res.send('Write code to retrieve all roles');
+  db('roles').then(roles => {
+    res.status(200).json(roles);
+  })
+  .catch(error=> {
+    res.status(500).json(error); 
+  })
 });
 
 router.get('/:id', (req, res) => {
